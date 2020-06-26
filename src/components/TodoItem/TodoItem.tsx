429062651da08dcc3a97a9e620c9182e132b7fd9
Yagogc <jagogc@gmail.com>
2020-06-26 21:56:55 +0100

    Add todo content logic

--- src/components/TodoItem/TodoItem.tsx
+++ src/components/TodoItem/TodoItem.tsx
@@ -1,9 +1,20 @@
-import React from 'react'
+import React, { useContext } from 'react'
+import { TodoContext } from '../../state/TodoContext'
 
 const TodoItem: React.FC<{ id: string }> = ({ id }) => {
+  const {
+    todos: { [id]: todo },
+    setTodoContent,
+  } = useContext(TodoContext)
+  const { content } = todo
   return (
     <li>
-      <input type="text" name="" id={id} />
+      <input
+        type="text"
+        id={id}
+        value={content}
+        onChange={(e) => setTodoContent(id, e.target.value)}
+      />
     </li>
   )
 }
