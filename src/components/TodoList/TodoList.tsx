5013cf0ae0102df87b57edec14f014cf31d5d82d
Yagogc <jagogc@gmail.com>
2020-06-26 21:49:06 +0100

    Update TodoList with TodoItem

--- src/components/TodoList/TodoList.tsx
+++ src/components/TodoList/TodoList.tsx
@@ -1,13 +1,21 @@
 import React, { useContext } from 'react'
 import { TodoContext } from '../../state/TodoContext'
+import TodoItem from '../TodoItem'
 
 const TodoList = () => {
   const { todos } = useContext(TodoContext)
   console.log('TodoList -> todos', todos)
-  if (Object.keys(todos).length === 0) {
+  const todosIds = Object.keys(todos)
+  if (todosIds.length === 0) {
     return <p>Add your first TODO</p>
   }
-  return <ul></ul>
+  return (
+    <ul>
+      {todosIds.reverse().map((id) => {
+        return <TodoItem id={id} key={id} />
+      })}
+    </ul>
+  )
 }
 
 export default TodoList
