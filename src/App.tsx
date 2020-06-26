92da3fe1381a2fb70c2ee10382028a698cedd4fa
Yagogc <jagogc@gmail.com>
2020-06-26 21:37:34 +0100

    Add TodoList to App

--- src/App.tsx
+++ src/App.tsx
@@ -3,13 +3,14 @@ import GlobalStyles from './styles/GlobalStyles'
 import MainLayout from './layouts/MainLayout'
 import { TodoProvider } from './state/TodoContext'
 import AddTodo from './components/AddTodo'
+import TodoList from './components/TodoList'
 
 function App() {
   return (
     <TodoProvider>
       <GlobalStyles />
       <MainLayout>
-        <div>Todo List</div>
+        <TodoList />
         <AddTodo />
       </MainLayout>
     </TodoProvider>
