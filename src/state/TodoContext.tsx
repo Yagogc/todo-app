7de9b52f1e5796ff0ca529727f746535abda7286
Yagogc <jagogc@gmail.com>
2020-06-26 21:35:53 +0100

    Add TodoContext logic

--- src/state/TodoContext.tsx
+++ src/state/TodoContext.tsx
@@ -1,9 +1,62 @@
-import React from 'react'
+import React, { useState } from 'react'
 
-const TodoContext = React.createContext({})
+interface Todo {
+  content: string
+  done: boolean
+}
+
+type Todos = Record<string, Todo>
+
+interface TodoContextInterface {
+  todos: Todos
+  createTodo: () => void
+  deleteTodo: (id: string) => void
+  setTodoContent: (id: string, content: string) => void
+  toggleTodoDone: (id: string) => void
+}
+
+const useTodos = (): TodoContextInterface => {
+  const [todos, setTodos] = useState<Todos>({})
+
+  const createTodo = (): void => {
+    const id = `todoid_${new Date().getTime()}`
+
+    todos[id] = {
+      content: '',
+      done: false,
+    }
+    setTodos({ ...todos })
+  }
+
+  const deleteTodo = (id: string): void => {
+    delete todos[id]
+    setTodos({ ...todos })
+  }
+
+  const setTodoContent = (id: string, content: string): void => {
+    todos[id].content = content
+    setTodos({ ...todos })
+  }
+
+  const toggleTodoDone = (id: string): void => {
+    todos[id].done = !todos[id].done
+    setTodos({ ...todos })
+  }
+
+  return { todos, createTodo, deleteTodo, setTodoContent, toggleTodoDone }
+}
+
+const TodoContext = React.createContext<TodoContextInterface>({
+  todos: {},
+  createTodo: () => {},
+  deleteTodo: () => {},
+  setTodoContent: () => {},
+  toggleTodoDone: () => {},
+})
 
 const TodoProvider: React.FC = ({ children }) => {
-  return <TodoContext.Provider value={{}}>{children}</TodoContext.Provider>
+  const todos = useTodos()
+  return <TodoContext.Provider value={todos}>{children}</TodoContext.Provider>
 }
 
 export { TodoContext, TodoProvider }
